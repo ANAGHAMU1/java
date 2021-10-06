340b5e48997eded7f4c47071cd63ba5ace306bc2
ANAGHAMU1 <84377171+ANAGHAMU1@users.noreply.github.com>
2021-10-06 12:30:56 +0530

    Add files via upload

---
+++ EXAM/Exception.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Scanner;
+class Product
+{
+int a[i],i;
+int product_code;
+String product_name;
+int price;
+try
+{
+void read()
+{
+DataInputStream dp=new DataInputStream();
+product_code=dp.readInt();
+product_name=dp.parseInt();
+price=dp.readInt();
+}
+Scanner sc=new Scanner();
+for(i=0;i<4;i++)
+{
+a[i] sc= new a[4];
+System.out.println("\nlength of the code is lessthan 4:",+a);
+}
+void display()
+{
+System.out.println("enter the product code :",+pcode);
+System.out.println("enter the product name :",+pname);
+System.out.println("enter the product price :",+pprice);
+}
+class Exception
+{
+int N;
+public static void main(String args[]) IOException()
+{
+Exception N=new Exception();
+N.read();
+N.display();
+}
+}}
+catch() 
+{
+System.out.println("Exception:");
+}
+}

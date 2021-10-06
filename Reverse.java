5cdcaf436b1854e96badcf45356affa8e658241c
ANAGHAMU1 <84377171+ANAGHAMU1@users.noreply.github.com>
2021-10-06 12:22:41 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+class Reverse
+{
+public static void main(String args[])
+{
+int num,rev;
+Scanner sc=new Scanner();
+sc.num();
+num=687;
+rev=num;
+System.out.println("\nreverse num",rev);
+}
+}
